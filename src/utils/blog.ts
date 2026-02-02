f336c7431caaf65a00cd2c76d5c779f5aa43002e
kafkiano <34920220+kafkiano@users.noreply.github.com>
2026-02-02 18:36:32 +0100

    fixed blog paths

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -60,7 +60,7 @@ const getNormalizedPost = async (post: CollectionEntry<'post'>): Promise<Post> =
     metadata = {},
   } = data;
 
-  const slug = cleanSlug(id); // cleanSlug(rawSlug.split('/').pop());
+  const slug = cleanSlug(id.split('/').pop() || id); // cleanSlug(rawSlug.split('/').pop());
   const publishDate = new Date(rawPublishDate);
   const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
 
